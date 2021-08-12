451e6a7fa6fc5f07431eea696c9fd0fa97d32c73
andreagt3 <78096470+RamonSGT@users.noreply.github.com>
2021-08-12 10:29:51 -0500

    Changed urls and deleted footer

--- assets/js/api.js
+++ assets/js/api.js
@@ -6,22 +6,6 @@
 //     }
 // })
 
-async function andrea(id) {
-    return await $.ajax({
-        url: 'https://tests-faas.azurewebsites.net/api/prueba?code=dtRLzM6kb5xasJ6k9rHls202wlHgqLFZBCFv1P6zFz5XtncsSBmSEA==',
-        method: 'GET',
-        data: JSON.stringify({ name: andrea }),
-        contentType: 'application/json',
-    })
-    .then(res => {
-        console.log(res)
-    })
-    .catch(error => {
-        return { error: { message: error.responseJSON?.message } }
-    })
-}
-andrea()
-
 async function getRates(type) {
     return await $.ajax({
         url: `https://jsfn-stech.azurewebsites.net/api/cfe/rates/${type}?code=pppBiG10avvQGWropiQjBOagsQ0rokLIqsBhCdpgWsMwPZRadSlXUA==`
@@ -83,3 +67,24 @@ async function getConsumptionsByProduct(id) {
     })
 }
 
+async function getConsumptionsByProduct(id) {
+    return await $.ajax({
+        url: 'https://jsfn-stech.azurewebsites.net/api/products/consumptions?code=pppBiG10avvQGWropiQjBOagsQ0rokLIqsBhCdpgWsMwPZRadSlXUA==',
+        method: 'POST',
+        data: JSON.stringify({ product: id }),
+        contentType: 'application/json',
+    })
+    .then(res => {
+        const { consumos } = JSON.parse(res)
+        let compare = (a, b) => {
+            if (a.porcentaje_trabajo < b.porcentaje_trabajo) return -1
+            if (a.porcentaje_trabajo > b.porcentaje_trabajo) return 1
+            return 0
+        }
+        consumos.sort(compare)
+        return consumos
+    })
+    .catch(error => {
+        return { error: { message: error.responseJSON?.message } }
+    })
+}
