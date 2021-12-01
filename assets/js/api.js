1c09c696c08f132c030c0114900fc510ad43f25c
eladio.rocha@sagaon.tech <eladio.rocha@sagaon.tech>
2021-11-30 22:28:57 -0600

    Changes

--- assets/js/api.js
+++ assets/js/api.js
@@ -113,9 +113,8 @@ async function generatePdf() {
   if(cutOrEngraveActived) {
     powerRate = document.querySelector("#listaConsumosSelect").value.toString().split("-").shift().trim()
     timePerDesign = document.querySelector("#horasTrabajoMaquina").value.toString()
-    currentRate = "Corriente eléctrica " + document.querySelector("#corrienteMax").value
-    console.log("El valor es: ", document.querySelector("#corrienteMax").value)
-    power = "Potencia " + document.querySelector("#power").value
+    currentRate = "Corriente eléctrica " + document.querySelector("#corrienteMax").innerText
+    power = "Potencia " + document.querySelector("#power").innerText
   } else if(!cutOrEngraveActived) {
     powerRate += "Corte " + document.querySelector("#listaConsumosSelect-2").value.toString().split("-").shift().trim()
     powerRate += " Grabado " + document.querySelector("#listaConsumosSelect-3").value.toString().split("-").shift().trim()
